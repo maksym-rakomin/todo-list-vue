33518ec6dca853ca694b2370a8eebc0882772cdc
maksym-rakomin <49858760+maksym-rakomin@users.noreply.github.com>
2020-03-19 16:15:26 +0200

    первый коммит

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo Vue Application</h1>
+    <hr>
+    <TodoList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from '@/components/todolist'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList
   }
 }
 </script>
---
+++ src/components/todolist.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <ul>
+          <li>todo 1</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
